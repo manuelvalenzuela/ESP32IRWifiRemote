7c9b9675b02d6da539a5706cf521b12ead1093df
Manuel Valenzuela <manolovalenzuelavega@gmail.com>
2022-01-24 19:28:23 -0800

    first work session

---
+++ IRWifiRemote.ino
@@ -0,0 +1,62 @@
+#include <LittleFS.h>
+#include <ArduinoJson.h>
+
+DynamicJsonDocument doc(200);
+
+#define FORMAT_LITTLEFS_IF_FAILED false
+
+class Config{
+  public:
+    const char* deviceMode;
+    const char* ssid;
+    const char* password;
+    Config(const char* deviceMode, const char* ssid, const char* password)
+    {
+      this->deviceMode = deviceMode;
+      this->ssid = ssid;
+      this->password = password;
+    }
+};
+
+Config readConfig(fs::FS &fs){
+  File file = fs.open("/config.json");
+  deserializeJson(doc, file);
+  const char* deviceMode = doc["mode"];
+  const char* ssid = doc[deviceMode]["ssid"];
+  const char* password = doc[deviceMode]["password"];
+
+  Config configuration = Config(deviceMode, ssid, password);
+  file.close();
+
+  return configuration;
+}
+
+void setup() {
+  Serial.begin(115200);
+  if(!LittleFS.begin(FORMAT_LITTLEFS_IF_FAILED))
+  {
+    Serial.println("LittleFS Mount failed");
+    return;
+  }
+
+  Serial.println("LittleFS Mount successful");
+  Serial.println("-.-");
+  Config configuration = readConfig(LittleFS);
+
+  Serial.printf("mode=%s\n", configuration.deviceMode);
+  Serial.printf("ssid=%s\n", configuration.ssid);
+  Serial.printf("password=%s\n", configuration.password);
+
+  LittleFS.end();
+
+
+  // look at the mode and decide what to do
+  // if is hotspot then create the hotspot
+  // create web to configure the router
+  // else connect to router
+  
+}
+
+void loop() {
+  
+}
